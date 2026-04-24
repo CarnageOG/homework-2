5ec9c74adff53bb3986ef1fda72858dc2618a264
CarnageOG <egetashviligiorgi9@gmail.com>
2026-04-24 15:31:10 +0400

    made fetch and added some styles

---
+++ src/app/page.js
@@ -0,0 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+
+export default function home() {
+  const [products, setProducts] = useState ([]);
+  const [loading, setLoading] = useState (true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    try{
+      fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((result) => setProducts(result));
+    } catch (error){
+      setError(true)
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  if(loading) {
+    return(
+      <div>Loading</div>
+    )
+  };
+
+  if(error) {
+    return(
+      <div>Something went wrong</div>
+    )
+  };
+
+ return (
+    <div className={styles.div_wrapper}>
+      {products.map((product) => (
+        <div className={styles.div_products} key={product.id}>
+          <h1 className={styles.product_title}>{product.title}</h1>
+          <p>{product.price} $</p>
+          <p className={styles.product_des}>{product.description}</p>
+          <img className={styles.product_img} src={product.image} alt={product.title} />
+          <div className={styles.product_category}>{product.category}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
