bfa548e8d93244bae48019b69026ad9b3513db96
CarnageOG <egetashviligiorgi9@gmail.com>
2026-04-26 23:53:41 +0400

    update

--- src/app/page.js
+++ src/app/page.js
@@ -35,15 +35,6 @@ export default function home() {
 
   return (
     <div className={styles.div_wrapper}>
-      {products.map((product) => (
-        <div className={styles.div_products} key={product.id}>
-          <h1 className={styles.product_title}>{product.title}</h1>
-          <p>Price: {product.price} $</p>
-          <p className={styles.product_des}>{product.description}</p>
-          <img className={styles.product_img} src={product.image} alt={product.title} />
-          <div className={styles.product_category}>{product.category}</div>
-        </div>
-      ))}
     </div>
   );
 }
